7e5a2841d2f36cd7f2218d8e4cfe854b527e151f
mexchy1000 <mexchy1000@naver.com>
2020-02-28 17:10:04 +0900

    Update spade_spatial_marker_by_deeplearning.py

--- spade_spatial_marker_by_deeplearning.py
+++ spade_spatial_marker_by_deeplearning.py
@@ -41,7 +41,7 @@ if __name__ == "__main__":
     parser.add_argument('--scale', type=float)
     parser.add_argument('--meta', type=str)
     parser.add_argument('--outdir', type=str, default='./SPADE_output/')
-    parser.add_argument('--numpcs', type=float, default= 2)
+    parser.add_argument('--numpcs', type=int, default= 2)
     args = parser.parse_args()
     
     if not os.path.exists(args.outdir):
